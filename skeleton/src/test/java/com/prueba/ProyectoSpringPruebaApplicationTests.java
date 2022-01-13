50427da4bf673dbc2accd881ba41d468cfc0fa18
AbderrahmaneGuermat <agb139@alu.ua.es>
2022-01-13 13:33:44 +0100

    vamos a probar descargar

--- skeleton/src/test/java/com/prueba/ProyectoSpringPruebaApplicationTests.java
+++ skeleton/src/test/java/com/prueba/ProyectoSpringPruebaApplicationTests.java
@@ -3,7 +3,7 @@ package com.prueba;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes=ProyectoSpringPruebaApplicationTests.class)
 class ProyectoSpringPruebaApplicationTests {
 
 	@Test
